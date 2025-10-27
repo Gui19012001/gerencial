ddcffdd3bd020162e1d50bdfd87cd45fa3cc1ac7
Gui19012001 <guilherme.cruz@iberoequipamentos.com.br>
2025-10-27 10:46:28 -0300

    Update TESTE.py

--- TESTE.py
+++ TESTE.py
@@ -265,7 +265,7 @@ def painel_dashboard():
         if screen_height >= 2000:
             pareto_height = 100  # TVs grandes 4K
         elif screen_height >= 1080:
-            pareto_height = 220  # TV full HD
+            pareto_height = 230  # TV full HD
         else:
             pareto_height = 400  # notebook ou monitor menor
 
