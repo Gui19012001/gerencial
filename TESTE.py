dc5f6f1d359b4dd9bf440d2cbee5b87e24929d91
Gui19012001 <guilherme.cruz@iberoequipamentos.com.br>
2025-10-27 11:19:10 -0300

    Atualizar o TESTE.py

--- TESTE.py
+++ TESTE.py
@@ -184,7 +184,7 @@ def painel_dashboard():
 
     # ======= Cartões Resumo + Gauge =======
     col1, col2, col3, col4 = st.columns(4)
-    altura = 170
+    altura = 150
     fonte = "18px"
 
     with col1:
